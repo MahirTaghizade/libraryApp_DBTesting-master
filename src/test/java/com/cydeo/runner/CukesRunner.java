8eddff8c23cc0308b06629e263ff1155504e7b41
mahirtaghizade <mahir95.x@icloud.com>
2022-07-30 14:47:51 -0400

    updates

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
         dryRun = false,
+        publish = true,
         tags = "@wip"
 )
 public class CukesRunner {
--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,3 +1,6 @@
 select count(*) from users;
 select count(*) from books;
 select count(*) from book_borrow;
+
+
+select name from book_categories;
